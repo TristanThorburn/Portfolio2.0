9dbd6a0eb96fc53928a8e494034352d5f4976e09
Tristan Thorburn <tristanthorburn@gmail.com>
2022-11-23 17:33:15 -0500

    finished HTML, nav CSS, hamburger menu JS

--- script/app.js
+++ script/app.js
@@ -1,1 +1,35 @@
-console.log(`hello world`)
+const hamburger = {};
+
+hamburger.app = () => {
+    const symbol = document.querySelector('.hamburger');
+    const menu = document.querySelector('.desktopNav');
+    const bars = document.querySelectorAll('.bar');
+    const links = document.querySelectorAll('.navLink');
+
+    hamburger.hoverTrue = symbol.addEventListener('mouseover', () => {
+        bars.forEach(bar => {
+            bar.style.backgroundColor = `rgb(73, 182, 24)`
+        });
+    });
+    hamburger.hoverFalse = symbol.addEventListener('mouseleave', () => {
+        bars.forEach(bar => {
+            bar.style.backgroundColor = `rgb(255, 255, 255)`
+        });
+    });
+    hamburger.openMenu = symbol.addEventListener('click', () => {
+        symbol.classList.toggle('active')
+        menu.classList.toggle('active')
+    });
+    hamburger.closeMenu = links.forEach(link => {
+        link.addEventListener('click', () => {
+            symbol.classList.remove('active');
+            menu.classList.remove('active');
+        });
+    });
+}
+
+hamburger.init = () => {
+    hamburger.app();
+}
+
+hamburger.init();
