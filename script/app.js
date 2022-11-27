a8d178f064df83f05ac21fb1fcd3c8a6a5d38948
Tristan Thorburn <tristanthorburn@gmail.com>
2022-11-26 20:59:47 -0500

    My first accordion child of an accordion JS

--- script/app.js
+++ script/app.js
@@ -1,3 +1,4 @@
+// MOBILE HAMBURGER MENU
 const hamburger = {};
 
 hamburger.app = () => {
@@ -33,3 +34,81 @@ hamburger.init = () => {
 }
 
 hamburger.init();
+
+//PROJECTS JAVASCRIPT
+const projects = {};
+
+projects.app = () => {
+    const projectImage = document.querySelectorAll('.projectImage');
+
+    projectImage.forEach(project => {
+        project.addEventListener('click', (e) => {
+            if(window.innerWidth < 1200){
+                project.classList.toggle('active')
+                
+                    const projectDetails = e.target.parentNode.parentElement.nextElementSibling;
+                    if(e.target.parentNode.parentElement.classList.contains("active")){
+                        projectDetails.style.maxHeight = projectDetails.scrollHeight + "px";
+                    }
+                    else{
+                        projectDetails.style.maxHeight = 0;
+                    }
+                }
+                else{
+                    project.removeEventListener("click", e);
+                }
+        });
+    });
+}
+
+projects.init = () => {
+    projects.app();
+}
+
+projects.init();
+// ARCHIVE JS
+const archive = {};
+
+archive.app = () => {
+    const archiveButton = document.querySelector('.archive');
+    const archiveContent = document.querySelector('.archiveContent');
+
+    archiveButton.addEventListener('click', (e) => {
+        archiveButton.classList.toggle('selected');
+
+        const eggProject = e.target.nextElementSibling.children[1].children[3];
+
+            if(archiveButton.classList.contains('selected')){
+                let archiveSize = archiveContent.scrollHeight;
+
+                archiveContent.style.maxHeight = archiveSize + 'px';
+
+                eggProject.addEventListener('click', (e) => {
+                    e.target.parentElement.classList.toggle('cracked');
+
+                    const scrambled = e.target.parentElement;
+                        if(scrambled.classList.contains('cracked') && window.innerWidth < 1200){
+                           const dinner = scrambled.parentElement.nextElementSibling.scrollHeight;
+                           const sideDish = scrambled.parentElement.nextElementSibling
+                           archiveSize = archiveSize + dinner;
+                           archiveContent.style.maxHeight = archiveSize + 'px'
+                           
+                           sideDish.style.maxHeight = sideDish.scrollHeight + 'px'
+                        }
+                        else{
+                            const sideDish = scrambled.parentElement.nextElementSibling
+                            sideDish.style.maxHeight = 0
+                        }
+                })
+            }
+            else{
+                archiveContent.style.maxHeight = 0;
+            }
+    });
+}
+
+archive.init = () => {
+    archive.app();
+}
+
+archive.init();
